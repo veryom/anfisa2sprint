a9261bc4cd180f8caa495cf8d8253a6f0b230dbf
Viacheslav Eremenko <veryom@gmail.com>
2025-01-08 18:58:14 +0300

    Add admin site

--- anfisa_for_friends/core/models.py
+++ anfisa_for_friends/core/models.py
@@ -2,8 +2,11 @@ from django.db import models
 
 
 class PublishedModel(models.Model):
-    """Абстрактная модель. Добвляет флаг is_published."""
-    is_published = models.BooleanField(default=True)
+    """Абстрактная модель. Добвляет флаги is_published и title."""
+    is_published = models.BooleanField(
+        default=True,
+        verbose_name='Опубиковано'
+    )
 
     class Meta:
         abstract = True
--- anfisa_for_friends/ice_cream/models.py
+++ anfisa_for_friends/ice_cream/models.py
@@ -4,35 +4,98 @@ from core.models import PublishedModel
 
 
 class Category(PublishedModel):
-    title = models.CharField(max_length=256)
-    slug = models.SlugField(max_length=64, unique=True)
-    output_order = models.PositiveSmallIntegerField(default=100)
+    title = models.CharField(
+        max_length=256,
+        verbose_name='Название',
+        help_text='Уникальное название категории, не более 256 символов'
+    )
+    slug = models.SlugField(
+        max_length=64,
+        unique=True,
+        verbose_name='Слаг'
+    )
+    output_order = models.PositiveSmallIntegerField(
+        default=100,
+        verbose_name='Порядок отображения'
+    )
+
+    class Meta:
+        verbose_name = 'объект "Категория"'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.title
 
 
 class Topping(PublishedModel):
-    title = models.CharField(max_length=256)
-    slug = models.SlugField(max_length=64, unique=True)
+    title = models.CharField(
+        max_length=256,
+        verbose_name='Название',
+        help_text='Уникальное название топпинга, не более 256 символов'
+    )
+    slug = models.SlugField(
+        max_length=64,
+        unique=True,
+        verbose_name='Слаг'
+    )
+
+    class Meta:
+        verbose_name = 'объект "Топпинг"'
+        verbose_name_plural = 'Топпинги'
+
+    def __str__(self):
+        return self.title
 
 
 class Wrapper(PublishedModel):
-    title = models.CharField(max_length=256)
+    title = models.CharField(
+        max_length=256,
+        verbose_name='Название',
+        help_text='Уникальное название обёртки, не более 256 символов'
+    )
+
+    class Meta:
+        verbose_name = 'объект "Обёртка"'
+        verbose_name_plural = 'Обёртки'
+
+    def __str__(self):
+        return self.title
 
 
 class IceCream(PublishedModel):
-    title = models.CharField(max_length=256)
-    description = models.TextField()
+    title = models.CharField(
+        max_length=256,
+        verbose_name='Название',
+        help_text='Уникальное название мороженого, не более 256 символов'
+    )
+    description = models.TextField(verbose_name='Описание')
     wrapper = models.OneToOneField(
         Wrapper,
         on_delete=models.SET_NULL,
         related_name='ice_cream',
         null=True,
         blank=True,
+        verbose_name='Обёртка'
     )
     category = models.ForeignKey(
         Category,
         on_delete=models.CASCADE,
         related_name='ice_creams',
+        verbose_name='Категория'
     )
-    toppings = models.ManyToManyField(Topping)
-    is_on_main = models.BooleanField(default=False)
+    toppings = models.ManyToManyField(
+        Topping,
+        verbose_name='Топпинг'
+    )
+    is_on_main = models.BooleanField(
+        default=False,
+        verbose_name='На главную'
+    )
+
+    class Meta:
+        verbose_name = 'объект "Мороженое"'
+        verbose_name_plural = 'Мороженое'
+
+    def __str__(self):
+        return self.title
 
