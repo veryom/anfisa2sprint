e3a70049bb06a4524812ecea51e6aef1fd8ae80f
Viacheslav Eremenko <veryom@gmail.com>
2025-01-09 22:21:13 +0300

    Add code in ice_cream views

--- anfisa_for_friends/homepage/views.py
+++ anfisa_for_friends/homepage/views.py
@@ -4,12 +4,14 @@ from ice_cream.models import IceCream
 
 def index(request):
     template = 'homepage/index.html'
-    ice_cream_list = IceCream.objects.values(
-        'id', 'title', 'description'
-        ).filter(
+    ice_cream_list = (
+        IceCream.objects.values('id', 'title', 'description')
+        .filter(
             is_published=True,
             is_on_main=True
         )
+        .order_by('title')[0:3]
+    )
     context = {
         'ice_cream_list': ice_cream_list
     }
