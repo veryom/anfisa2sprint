10c3a18c5fac11f3ba28c4513ee8c081a8c66d18
Viacheslav Eremenko <veryom@gmail.com>
2025-01-09 17:07:49 +0300

    Add queries and filters

--- anfisa_for_friends/homepage/views.py
+++ anfisa_for_friends/homepage/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
+from ice_cream.models import IceCream
 
 
 def index(request):
     template = 'homepage/index.html'
-    return render(request, template)
+    ice_cream_list = IceCream.objects.values(
+        'id', 'title', 'description'
+        ).filter(
+            is_published=True,
+            is_on_main=True
+        )
+    context = {
+        'ice_cream_list': ice_cream_list
+    }
+    return render(request, template, context)
